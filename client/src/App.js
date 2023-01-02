3fb4821c6c3fb3193a105960cb30ea8dec3e93f5
Alex <alexmore080@gmail.com>
2023-01-02 12:45:05 -0600

    set up app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { NextUIProvider } from '@nextui-org/react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+  createHttpLink,
+} from '@apollo/client';
+
 import './App.css';
+import Header from './components/header';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <NextUIProvider>
+        <Router>
+          <Header />
+        </Router>
+      </NextUIProvider>
+    </ApolloProvider>
   );
 }
 
