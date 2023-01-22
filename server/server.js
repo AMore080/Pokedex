8defc0e21adf97cadf253775f989b0e6e684b050
Alex <alexmore080@gmail.com>
2023-01-21 22:22:36 -0600

    server route change

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
+const path = require('path');
 
 
 // utils
@@ -23,6 +24,10 @@ const server = new ApolloServer({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 const startApolloServer = async (typeDefs, resolvers) => {
     await server.start();
