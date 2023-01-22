70f61b085e9584b6db217c939a4e21f4967d66bf
Alex <alexmore080@gmail.com>
2023-01-21 22:43:59 -0600

    server changes

--- server/server.js
+++ server/server.js
@@ -23,7 +23,11 @@ const server = new ApolloServer({
 })
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));\
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
